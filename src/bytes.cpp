bf761e2155697f018c14c093a8c2557e728dcc73
Alex Oxorn <alex.oxorn@gmail.com>
2021-07-01 21:34:47 -0400

    bug_fix

--- src/bytes.cpp
+++ src/bytes.cpp
@@ -2,16 +2,16 @@
 
 namespace ox {
     void swap(void* data, int size, int length) {
-        for(int offset = 0; i < size; offset++) {
+        for(int offset = 0; offset < length; offset++) {
             switch(size) {
             case sizeof(u16):
-                ox::swap(reinterpret_cast<u16*>(buffer) + offset);
+                ox::swap(reinterpret_cast<u16*>(data) + offset);
                 break;
             case sizeof(u32):
-                ox::swap(reinterpret_cast<u32*>(buffer) + offset);
+                ox::swap(reinterpret_cast<u32*>(data) + offset);
                 break;
             case sizeof(u64):
-                ox::swap(reinterpret_cast<u64*>(buffer) + offset);
+                ox::swap(reinterpret_cast<u64*>(data) + offset);
                 break;
             }
         }
