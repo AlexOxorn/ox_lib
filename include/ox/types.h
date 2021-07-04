8d9a28325dafa8959db87056c81b5a23db660751
Alex Oxorn <alex.oxorn@gmail.com>
2021-07-03 21:41:22 -0400

    add new typetrait

--- include/ox/types.h
+++ include/ox/types.h
@@ -2,6 +2,8 @@
 #define _OXORN_TYPES_H
 
 #include <stdint.h>
+#include <type_traits>
+#include <concepts>
 
 typedef uint8_t  u8;
 typedef uint16_t u16;
@@ -12,5 +14,21 @@ typedef int8_t  s8;
 typedef int16_t s16;
 typedef int32_t s32;
 typedef int64_t s64;
+
+namespace ox {
+    template<typename T>
+    struct is_endianable : std::integral_constant<
+        bool,
+        std::is_arithmetic_v<T> ||
+        std::is_pointer_v<T> ||
+        std::is_enum_v<T>
+    > {};
+
+    template<typename T>
+    using is_endianable_v<T> = is_endianable<T>::value;
+
+    concept endianable = is_endianable_v<T>;
+}
+
 
 #endif
