b01f1da184b182ebdb25ce982dc18d11f6b90902
Alex Oxorn <alex.oxorn@gmail.com>
2022-12-02 20:46:19 -0500

    day 02

---
+++ include/math/_modulo.h
@@ -0,0 +1,74 @@
+//
+// Created by alexoxorn on 12/2/22.
+//
+
+#pragma once
+#include <concepts>
+
+#define OP_ASSIGNMENT(op, T, val) \
+  T& operator op##=(const T& other) { \
+    val op## = other.val; \
+    update(); \
+    return *this; \
+  } \
+  T& operator op##=(const std::integral auto& other) { \
+    val op## = other; \
+    update(); \
+    return *this; \
+  }
+#define OPERATOR_EXPANSION(op, T) \
+  T operator op(const T& other) const { \
+    T ret{*this}; \
+    ret op## = other; \
+    return ret; \
+  } \
+  T operator op(const std::integral auto& other) const { \
+    T ret{*this}; \
+    ret op## = other; \
+    return ret; \
+  }
+
+namespace ox {
+    template <size_t M, std::integral T = std::size_t>
+    struct modulo {
+        T result;
+
+        modulo() = default;
+        modulo(std::integral auto in) : result(in){ update(); };
+
+        OP_ASSIGNMENT(+, modulo, result);
+        OPERATOR_EXPANSION(+, modulo)
+        OP_ASSIGNMENT(-, modulo, result);
+        OPERATOR_EXPANSION(-, modulo)
+        OP_ASSIGNMENT(*, modulo, result);
+        OPERATOR_EXPANSION(*, modulo)
+
+        modulo& operator++() { return *this += 1; }
+        modulo& operator--() { return *this -= 1; }
+
+        modulo operator++(int) {
+            modulo ret{*this};
+            *this += 1;
+            return ret;
+        }
+        modulo operator--(int) {
+            modulo ret{*this};
+            *this -= 1;
+            return ret;
+        }
+
+        operator T() { return result; }
+    private:
+        void update() {
+            if (result >= static_cast<T>(M)) {
+                result %= M;
+            }
+            while (result < 0) {
+                result += M;
+            }
+        }
+    };
+} // namespace ox
+
+#undef OP_ASSIGNMENT
+#undef OPERATOR_EXPANSION
--- include/ox/math.h
+++ include/ox/math.h
@@ -6,5 +6,6 @@
 #define OX_LIB_MATH_H
 
 #include "math/_simple_constexpr_math.h"
+#include "math/_modulo.h"
 
 #endif //OX_LIB_MATH_H
