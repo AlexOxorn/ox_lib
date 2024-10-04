a7ffcc3f31726c66c22c7934ae67fc8741ca6a19
Alex Oxorn <alex.oxorn@gmail.com>
2024-10-03 23:07:55 -0400

    Solved Day 25 w/ dikstra

--- include/graph/_dikstra.h
+++ include/graph/_dikstra.h
@@ -22,6 +22,9 @@ namespace ox {
     };
 
     struct a_start {};
+    struct full_dikstra {};
+    struct sentinel_default {};
+    struct neighbour_default {};
     struct heuristic_default {};
 
     template <
@@ -29,10 +32,10 @@ namespace ox {
             typename Node,
 
             // Type representing the final Node in path searching
-            typename Sentinel,
+            typename Sentinel = sentinel_default,
 
             // To represent the function to get heuristic measurements
-            typename NeighbourFunction,
+            typename NeighbourFunction = neighbour_default,
 
             // To represent the function to get heuristic measurements
             typename Heuristic = heuristic_default,
@@ -45,6 +48,7 @@ namespace ox {
     class dikstra_solver {
     public:
         using NodeType = Node;
+        using SentinelType = Sentinel;
         using NeighbourRange = decltype(std::invoke(std::declval<NeighbourFunction>(), std::declval<Node>()));
         using NeighbourStep = NeighbourRange::value_type;
         using Cost = NeighbourStep::second_type;
@@ -58,10 +62,14 @@ namespace ox {
     private:
         using DebugFunc = std::function<void(const NodeType&, const Cost&, const OpenSet&, const GScoreMap&,
                                              const DirectionalMap&)>;
-        constexpr static bool AStart = std::invocable<Heuristic, Node, Sentinel>;
+        constexpr static bool AStar = std::invocable<Heuristic, Node, Sentinel>;
+        constexpr static bool Full = !requires(Node n, Sentinel s) {
+            { n == s } -> std::convertible_to<bool>;
+            { n == n } -> std::convertible_to<bool>;
+        };
 
         Node start;
-        Sentinel end;
+        Sentinel sentinel;
         NeighbourFunction get_neighbours;
 
         Heuristic _heuristic;
@@ -72,18 +80,20 @@ namespace ox {
         DirectionalMap came_from;
         GScoreMap g_score;
 
+        ResultType result;
+
         DebugFunc _debug_func;
 
         bool track_came_from = false;
 
         Cost heuristic(const Node& a, const auto& b)
-        requires AStart && std::convertible_to<Heuristic, bool>
+        requires AStar && std::convertible_to<Heuristic, bool>
         {
             return _heuristic ? std::invoke(_heuristic, a, b) : 0;
         }
 
         Cost heuristic(const Node& a, const Sentinel& b)
-        requires AStart
+        requires AStar
         {
             return std::invoke(_heuristic, a, b);
         }
@@ -103,8 +113,8 @@ namespace ox {
                         came_from.insert_or_assign(neighbour, current);
                     g_score.insert_or_assign(neighbour, tentative_score);
                     Cost f_cost = tentative_score;
-                    if constexpr (AStart)
-                        f_cost += heuristic(neighbour, end);
+                    if constexpr (AStar)
+                        f_cost += heuristic(neighbour, sentinel);
                     open_set.push(std::make_pair(neighbour, f_cost));
                 }
             }
@@ -113,7 +123,14 @@ namespace ox {
         dikstra_solver(Node _start, Sentinel end, NeighbourFunction get_neighbours, Hash = Hash(),
                        CostComparison cmp = CostComparison()) :
                 start(std::move(_start)),
-                end(std::move(end)),
+                sentinel(std::move(end)),
+                get_neighbours(std::move(get_neighbours)),
+                cmp(cmp),
+                open_set(f_compare(cmp), std::vector{std::make_pair(start, Cost{})}) {}
+
+        dikstra_solver(full_dikstra, Node _start, NeighbourFunction get_neighbours, Hash = Hash(),
+                       CostComparison cmp = CostComparison()) :
+                start(std::move(_start)),
                 get_neighbours(std::move(get_neighbours)),
                 cmp(cmp),
                 open_set(f_compare(cmp), std::vector{std::make_pair(start, Cost{})}) {}
@@ -121,7 +138,7 @@ namespace ox {
         dikstra_solver(a_start, Node _start, Sentinel end, NeighbourFunction get_neighbours,
                        Heuristic heuristic_function, Hash = Hash(), CostComparison cmp = CostComparison()) :
                 start(std::move(_start)),
-                end(std::move(end)),
+                sentinel(std::move(end)),
                 get_neighbours(std::move(get_neighbours)),
                 _heuristic(std::move(heuristic_function)),
                 cmp(cmp),
@@ -129,9 +146,7 @@ namespace ox {
 
         void set_debug_func(DebugFunc&& f) { _debug_func = std::move(f); }
 
-        void track_path() {
-            track_came_from = true;
-        }
+        void track_path() { track_came_from = true; }
 
         ResultType generate_final_result(const Node& current) {
             ResultPath to_return{
@@ -146,40 +161,92 @@ namespace ox {
             return {to_return, g_score.at(current)};
         }
 
-        ResultType operator()()
-        requires requires(Node n, Sentinel s) {
-                     { n == s } -> std::convertible_to<bool>;
-                     { n == n } -> std::convertible_to<bool>;
-                 }
-        {
+        std::conditional<Full, void, ResultType>::type next() {
             g_score[start] = Cost{};
 
             while (!open_set.empty()) {
                 auto [current, current_cost] = open_set.top();
                 open_set.pop();
                 debug(current, current_cost, open_set, g_score, came_from);
-                if (current == end) {
-                    return generate_final_result(current);
+                if constexpr (Full) {
+                    result = generate_final_result(current);
+                    process_neighbours(current);
+                    return;
+                } else {
+                    if (current == sentinel) {
+                        return generate_final_result(current);
+                    }
+                    process_neighbours(current);
                 }
+            }
+            if constexpr (!Full)
+                return {};
+            else
+                result = {};
+        }
 
-                process_neighbours(current);
+        ResultType operator()()
+        requires(!Full)
+        {
+            return next();
+        }
+
+        void reset() {
+            g_score.clear();
+            came_from.clear();
+            result = {};
+            if constexpr (AStar) {
+                open_set = OpenSet(f_compare(cmp), std::vector{std::make_pair(start, heuristic(start, sentinel))});
+            } else {
+                open_set = OpenSet(f_compare(cmp), std::vector{std::make_pair(start, Cost{})});
+            }
+        }
+
+        auto begin()
+        requires Full
+        {
+            reset();
+            next();
+            return iterator{this};
+        }
+
+        auto end()
+        requires Full
+        {
+            return iterator_sentinel{};
+        }
+
+        struct iterator_sentinel {};
+        struct iterator {
+            dikstra_solver* base;
+            using difference_type = std::ptrdiff_t;
+            using value_type = ResultType;
+
+            ResultType operator*() const { return base->result; };
+
+            iterator& operator++() { base->next(); return *this; };
+
+            iterator operator++(int) {
+                auto tmp = *this;
+                ++*this;
+                return tmp;
             }
 
-            return {};
-        }
+            bool operator==(const iterator_sentinel&) const { return base->result.first.empty(); };
+        };
     };
 
     template <typename Node, typename GetNeighbours, typename Sentinel = Node,
               typename Heuristic = decltype([](...) { return 0; }), typename Hash = std::hash<Node>,
               typename DebugFunc = decltype([](const Node&) {})>
     requires requires(GetNeighbours g, Heuristic h, Node iter, Sentinel sent) {
-                 { g(iter) } -> std::ranges::range;
-                 { g(iter).begin()->first } -> std::convertible_to<Node>;
-                 { g(iter).begin()->second } -> std::totally_ordered;
-                 { h(iter, iter) } -> std::integral;
-                 { h(iter, sent) } -> std::integral;
-                 { iter == sent } -> std::convertible_to<bool>;
-             }
+        { g(iter) } -> std::ranges::range;
+        { g(iter).begin()->first } -> std::convertible_to<Node>;
+        { g(iter).begin()->second } -> std::totally_ordered;
+        { h(iter, iter) } -> std::integral;
+        { h(iter, sent) } -> std::integral;
+        { iter == sent } -> std::convertible_to<bool>;
+    }
     auto dikstra(Node start, Sentinel end, GetNeighbours get_neighbours_function, Heuristic heuristic_function = {},
                  Hash = Hash(), DebugFunc callback = DebugFunc())
             -> std::pair<std::vector<std::pair<Node, decltype(get_neighbours_function(start).begin()->second)>>,
